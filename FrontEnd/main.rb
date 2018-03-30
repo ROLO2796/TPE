43043dffffb6b846d2ba141edf0fe6bb42cae78b
Santiago Reyes <sreyes@itba.edu.ar>
2018-03-29 22:26:55 -0300

    implementacion de la entrada

--- FrontEnd/main.rb
+++ FrontEnd/main.rb
@@ -1,3 +1,19 @@
 until (a = gets.chomp) =~ /(?:ex|qu)it/i
   # maquina de estado que vaya buscando comandos y ejecute bloques de codigo.
+  line = a.split
+  puts line.to_s
+  # if line[0] == 'add'
+  #   puts 'grupo: ' + line[1][1..-1] + "\n" + 'tarea: ' + a[5..-1] if line[1][0] == '+'
+  #   puts 'El nombre de la tarea es: ' + a[line[0].size+line[1.size]+1..-1] if line[1][0] != '+'
+  # else
+  #   puts 'no enteindo'
+  # end
+  if line[0] == 'add'
+    if line[1][1] == '+'
+      grupo = line[1][1..-1]
+      tarea = a[(grupo.size + 4)..-1]
+    end
+  end
 end
+# la joda esta con las expresiones regulares y despues es cuestion de usar al string como un array
+# para guardar lo que queda.
